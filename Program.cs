78bba3f409b159f5edc31639471413cf4c7b4e8c
zabronax <zabronax@gmail.com>
2025-01-28 11:02:56 +0100

    setup main input handling

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿Console.WriteLine("Starting Renting Service");
 
-string? input = Console.ReadLine();
-if (input == null)
+while (true)
 {
-  Environment.Exit(1);
-}
+  string? input = Console.ReadLine();
+  if (input == null)
+  {
+    Environment.Exit(1);
+  }
 
-Console.WriteLine("Echo: " + input);
+  switch (input)
+  {
+    case "list":
+      Console.WriteLine("Listing out books");
+      break;
+    case "borrow":
+      Console.WriteLine("Borrowing book");
+      break;
+    case "return":
+      Console.WriteLine("Returning book");
+      break;
+    default:
+      Console.WriteLine("Invalid Input");
+      break;
+  }
+}
